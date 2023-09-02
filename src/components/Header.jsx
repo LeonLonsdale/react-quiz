a93c7241dafd4f65085204072cbe2d823c72179c
LeonLonsdale <leon.codes@icloud.com>
2023-09-02 20:31:40 +0100

    create Main component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import reactLogo from '../assets/logo512.png';
+
 function Header() {
   return (
     <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
+      <img src={reactLogo} alt='React logo' />
       <h1>The React Quiz</h1>
     </header>
   );
